24339376acb0f821c0daf8522261d194eb851529
栗城朋広 <kurikitomohiro@kurikitomohironoMacBook-Air.local>
2022-05-16 07:17:34 +0900

    PORT

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const io = new Server(server);
-const { Server } = require("socket.io"); 
+const io = require("socket.io")(server);
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
